1847fa8d7e07bb7472101a1d37a4fd0e03fe145e
AbdulMoyeen <moyeenkhan47@gmail.com>
2025-04-30 12:21:59 +0530

    first commit

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello this is testing code for jenkins");
         System.out.println("This is the java program ");
         int a=10;
         System.out.println(a);
+        System.out.println("Hello,Hi nmy name is Abdul");
     }
 }
